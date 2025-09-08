3483a6de3757f77732953e6598efd44514fe2565
Maria <mnastase@uwaterloo.ca>
2025-09-07 23:27:04 -0400

    homepage styling

--- maria.nastase/src/app/page.tsx
+++ maria.nastase/src/app/page.tsx
@@ -5,16 +5,16 @@ export default function Home() {
         <h2>2B Systems Design Engineering Student @ UWaterloo</h2>
         <h2>Interested in Industrial Automation and Robotics</h2>
         <div id="links">
-          <a id="resume" href="\Maria Nastase Resume.pdf" target="_blank"><img src="document.svg"></img></a>
-          <a id="linkedin" href="https://www.linkedin.com/in/marianastase/" target="_blank"><img src="linkedin.svg"></img></a>
-          <a id="github" href="https://github.com/maria-nastase" target="_blank"><img src="github.svg"></img></a>
-          <a id="email" href="mailto:mnastase@uwaterloo.ca"><img src="email.png"></img></a>
+          <div className="socials"><a id="resume" href="\Maria Nastase Resume.pdf" target="_blank"><img src="document.svg"></img></a></div>
+          <div className="socials"><a id="linkedin" href="https://www.linkedin.com/in/marianastase/" target="_blank"><img src="linkedin.svg"></img></a></div>
+          <div className="socials"><a id="github" href="https://github.com/maria-nastase" target="_blank"><img src="github.svg"></img></a></div>
+          <div className="socials"><a id="email" href="mailto:mnastase@uwaterloo.ca"><img src="email.png"></img></a></div>
+        </div>
         <div id="pages">
-          <a id="aboutme" href="\about">About Me</a>
-          <a id="projects" href="\projects">Projects</a>
+          <div className="page-button"><a id="aboutme" href="\about">About Me</a></div>
+          <div className="page-button"><a id="projects" href="\projects">Projects</a></div>
         </div>
       </div>
-    </div>
     );
   }
   
